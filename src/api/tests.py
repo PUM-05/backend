23acb4dd94ca09de662ee5f663a81c9bf5122211
Filip Johnsson <johnsson.filip@protonmail.com>
2023-02-16 11:30:11 +0100

    Add example test

--- src/api/tests.py
+++ src/api/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class APITests(TestCase):
+    def test_example(self):
+        response = self.client.get('/api/example')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), 'Hello World!')
