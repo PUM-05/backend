ab953aef495d3d093ceb2e230e9d2f231dc21e58
Filip Johnsson <johnsson.filip@protonmail.com>
2023-02-20 23:19:58 +0100

    Add all case paths

--- src/api/interfaces/cases.py
+++ src/api/interfaces/cases.py
@@ -1,4 +1,23 @@
+from typing import Any, Dict, List
 
-def get_case():
-    # Return dymmy data
-    return [{"id": 1, "medium": "phone"}]
+
+# TODO: Return type should be list of case models when they are implemented
+def get_case(parameters: Dict[str, Any]):
+    """
+    Returns all cases that match the given parameters.
+    """
+    # Return dummy data
+    return [{"id": 1, "medium": "phone", "notes": "Hello World!"},
+            {"id": 2, "medium": "email", "notes": "This is a test."}]
+
+
+# TODO: Return type should be list of category models when they are implemented
+def get_case_categories() -> List[Dict[str, Any]]:
+    """
+    Returns all case categories.
+    """
+    # Return dummy data
+    return [{"id": 1, "name": "category1"},
+            {"id": 2, "name": "category2"},
+            {"id": 3, "name": "category3"},
+            {"id": 4, "name": "category4"}]
