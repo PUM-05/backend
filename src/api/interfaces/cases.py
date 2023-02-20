4d258d35e5f67fa11620fcfb29abd09e10bbf985
Filip Johnsson <johnsson.filip@protonmail.com>
2023-02-20 17:00:12 +0100

    Setup paths for cases

---
+++ src/api/interfaces/cases.py
@@ -0,0 +1,4 @@
+
+def get_case():
+    # Return dymmy data
+    return [{"id": 1, "medium": "phone"}]
--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,27 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.views.decorators.http import require_http_methods
+import json
+
+from api.interfaces.cases import get_case
 
 
 def example(request):
     return HttpResponse("Hello World!")
+
+
+@require_http_methods({"GET", "POST"})
+def case(request: HttpRequest):
+    if request.method == "GET":
+        return HttpResponse(json.dumps(get_case()), content_type="application/json")
+    
+    elif request.method == "POST":
+        return HttpResponse()
+   
+
+@require_http_methods({"PATCH", "DELETE"})
+def case_id(request: HttpRequest, id: int):
+    if request.method == "PATCH":
+        return HttpResponse()
+
+    elif request.method == "DELETE":
+        return HttpResponse()
