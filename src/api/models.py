52e3e63cb90b5a9912c9d4f577139f901b6dd3a6
Filip Johnsson <johnsson.filip@protonmail.com>
2023-02-22 14:57:56 +0100

    Add models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField()
+
+
+class Customer(models.Model):
+    phone = models.CharField()
+    email = models.CharField()
+
+
+class Case(models.Model):
+    notes = models.CharField()
+    medium = models.CharField(max_length=50)
+    customer_time = models.DurationField()
+    additional_time = models.DurationField()
+    form_fill_time = models.DurationField()
+    created_at = models.DateTimeField()
+    edited_at = models.DateTimeField()
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
