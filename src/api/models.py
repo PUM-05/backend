1fdbc0cc2cdf2166031e6df763e21655c6f2dcd2
Filip Johnsson <johnsson.filip@protonmail.com>
2023-02-22 15:15:22 +0100

    Update models, install api app

--- src/api/models.py
+++ src/api/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=50)
 
 
 class Customer(models.Model):
-    phone = models.CharField()
-    email = models.CharField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
 
 
 class Case(models.Model):
-    notes = models.CharField()
+    notes = models.TextField()
     medium = models.CharField(max_length=50)
     customer_time = models.DurationField()
     additional_time = models.DurationField()
@@ -19,5 +20,6 @@ class Case(models.Model):
     created_at = models.DateTimeField()
     edited_at = models.DateTimeField()
 
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    user = models.ManyToManyField(User)
