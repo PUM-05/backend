7c85c122c7f55e823aa9826c09f3cf742c54020e
jacko460 <jacko460@student.liu.se>
2023-03-07 17:42:27 +0100

    django can now host the webserver

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from .views import index
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
+    #path('', index),
+    re_path(r'^(?!api|admin)(?!...$).*$', index)
 ]
