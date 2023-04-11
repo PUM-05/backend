d852546d0bac672e022371907b8972b4b37444fd
Anton Bergman <anton.u.bergman@gmail.com>
2023-04-11 10:16:56 +0200

    added CORS support and custom decorator for login

---
+++ src/api/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+
+def user_is_authenticated(user):
+    return user.is_authenticated
+
+
+def authentication_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not user_is_authenticated(request.user):
+            return HttpResponse(status=401)
+        return view_func(request, *args, **kwargs)
+    return wrapper
